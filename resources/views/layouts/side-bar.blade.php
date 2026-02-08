d4052a144858eddf873fe2d68245876ba7ff38fc
Md Shanto Hossain <mdshantohossain800@gmail.com>
2026-02-08 17:49:39 +0600

    fix sidebar module

--- resources/views/layouts/side-bar.blade.php
+++ resources/views/layouts/side-bar.blade.php
@@ -10,19 +10,13 @@
                 <li class="menu-title" key="t-menu">Menu</li>
 
                 <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                    <a href="{{ route('dashboard') }}" class="waves-effect">
                         <i class="bx bx-home-circle"></i>
                         <span key="t-dashboards">Dashboards</span>
                     </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="index.html" key="t-default">Default</a></li>
-                        <li><a href="dashboard-saas.html" key="t-saas">Saas</a></li>
-                        <li><a href="dashboard-crypto.html" key="t-crypto">Crypto</a></li>
-                        <li><a href="dashboard-blog.html" key="t-blog">Blog</a></li>
-                        <li><a href="dashboard-job.html" key="t-jobs">Jobs</a></li>
-                    </ul>
                 </li>
 
+                <!-- HRM -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-calendar"></i>
@@ -38,33 +32,51 @@
                     </ul>
                 </li>
 
-                <li>
-                    <a href="javascript: void(0);" class="has-arrow waves-effect">
-                        <i class="bx bx-store"></i>
-                        <span key="t-ecommerce">Operations</span>
-                    </a>
-                    <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="ecommerce-products.html" key="t-products">Products</a></li>
-                        <li><a href="ecommerce-product-detail.html" key="t-product-detail">Categories</a></li>
-                        <li><a href="ecommerce-orders.html" key="t-orders">Warehouse</a></li>
-                        <li><a href="ecommerce-customers.html" key="t-customers">Inventory Transactions</a></li>
-                    </ul>
-                </li>
-
+                <!-- INVENTORY -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bxs-user-detail"></i>
-                        <span key="t-contacts">Purchase & Sales</span>
+                        <span key="t-contacts">Inventory</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="contacts-grid.html" key="t-user-grid">Suppliers</a></li>
-                        <li><a href="contacts-list.html" key="t-user-list">Purchases</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Customers</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Sales / Invoices</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Payments</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Products</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Categories</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Warehouses</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Stock</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Adjustments</a></li>
+                        <li><a href="contacts-list.html" key="t-user-list">Transfers</a></li>
                     </ul>
                 </li>
 
+                <!-- SALES -->
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-store"></i>
+                        <span key="t-ecommerce">Sales</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="ecommerce-products.html" key="t-products">Customers</a></li>
+                        <li><a href="ecommerce-product-detail.html" key="t-product-detail">Orders </a></li>
+                        <li><a href="ecommerce-orders.html" key="t-orders">Invoice</a></li>
+                        <li><a href="ecommerce-customers.html" key="t-customers">Payments</a></li>
+                    </ul>
+                </li>
+
+                <!-- PURCHASE -->
+                <li>
+                    <a href="javascript: void(0);" class="has-arrow waves-effect">
+                        <i class="bx bx-bitcoin"></i>
+                        <span key="t-crypto">Purchase</span>
+                    </a>
+                    <ul class="sub-menu" aria-expanded="false">
+                        <li><a href="crypto-wallet.html" key="t-wallet">Suppliers</a></li>
+                        <li><a href="crypto-buy-sell.html" key="t-buy">Purchases Orders</a></li>
+                        <li><a href="crypto-exchange.html" key="t-exchange">Stock In</a></li>
+                        <li><a href="crypto-exchange.html" key="t-exchange">Bills</a></li>
+                    </ul>
+                </li>
+
+                <!-- FINANCE -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bx-bitcoin"></i>
@@ -74,24 +86,23 @@
                         <li><a href="crypto-wallet.html" key="t-wallet">Accounts</a></li>
                         <li><a href="crypto-buy-sell.html" key="t-buy">Transactions</a></li>
                         <li><a href="crypto-exchange.html" key="t-exchange">Expenses</a></li>
-                        <li><a href="crypto-exchange.html" key="t-exchange">Profit & loss report</a></li>
+                        <li><a href="crypto-exchange.html" key="t-exchange">Reports</a></li>
                     </ul>
                 </li>
 
-
+                <!-- BRANCHES MANAGEMENT -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bxs-user-detail"></i>
                         <span key="t-contacts">Branch management</span>
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
-                        <li><a href="contacts-grid.html" key="t-user-grid">System settings</a></li>
-                        <li><a href="contacts-list.html" key="t-user-list">Currency & tax</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Fiscal year</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Role permissions</a></li>
+                        <li><a href="contacts-grid.html" key="t-user-grid">Branches</a></li>
+                        <li><a href="contacts-list.html" key="t-user-list">Users</a></li>
                     </ul>
                 </li>
 
+                <!-- SETTINGS -->
                 <li>
                     <a href="javascript: void(0);" class="has-arrow waves-effect">
                         <i class="bx bxs-user-detail"></i>
@@ -99,9 +110,10 @@
                     </a>
                     <ul class="sub-menu" aria-expanded="false">
                         <li><a href="contacts-grid.html" key="t-user-grid">System settings</a></li>
-                        <li><a href="contacts-list.html" key="t-user-list">Currency & tax</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Fiscal year</a></li>
-                        <li><a href="contacts-profile.html" key="t-profile">Role permissions</a></li>
+                        <li><a href="contacts-list.html" key="t-user-list">Tax</a></li>
+                        <li><a href="contacts-profile.html" key="t-profile">Currency</a></li>
+                        <li><a href="contacts-profile.html" key="t-profile">Roles</a></li>
+                        <li><a href="contacts-profile.html" key="t-profile">Permissions</a></li>
                     </ul>
                 </li>
 
